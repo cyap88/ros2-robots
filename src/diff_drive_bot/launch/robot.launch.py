6ce79c6e7f36e5b5962921cb0ec452ae5a07a7a4
cyap88 <camille.yap2008@gmail.com>
2025-07-13 01:39:58 -0400

    wrote up  launch file that spawns robot in gz

--- src/diff_drive_bot/launch/robot.launch.py
+++ src/diff_drive_bot/launch/robot.launch.py
@@ -1,6 +1,51 @@
 import os
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfigurations
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
+from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
 
 def generate_launch_description():
     package = "diff_drive_bot"
+    world = LaunchConfiguration('world')
     urdf_path = os.path.join(get_package_share_directory(package), "urdf", "robot.urdf.xacro")
+    world_path = os.path.join(get_package_share_directory(package), "worlds", "empty.world")
+    
+    world_arg = DeclareLaunchArgument(
+        name='world', default_value=world_path, description='Load world'
+    )
+    
+    rsp = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory(package),'launch','rsp.launch.py'
+            )]), launch_arguments={'use_sim_time': 'true', 'urdf': urdf_path}.items()
+    )   
+    
+    gazebo = IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource([os.path.join(
+                        get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+                    )]), launch_arguments={'gz_args': [world], 'on_exit_shutdown': 'true'}.items()
+    )
 
+    spawn_bot = Node(
+            package='ros_gz_sim', 
+            executable='create',
+            arguments=['-topic', 'robot_description',
+                        '-name', 'robot',
+                        '-z', '0.2'],
+            output='screen'
+    ) 
+
+    bridge = os.path.join(get_package_share_directory(package),'config','gz_ros_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge}',]
+    )
+   
+    return LaunchDescription([world_arg, rsp, gazebo, ros_gz_bridge, spawn_bot])
---
+++ src/diff_drive_bot/launch/rsp.launch.py
@@ -0,0 +1,32 @@
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+
+def generate_launch_description():
+
+    package = FindPackageShare("diff_drive_bot")
+
+    urdf_path = PathJoinSubstitution([package, "urdf", "robot.urdf.xacro"])
+    
+    urdf = LaunchConfiguration('urdf')
+    
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time = DeclareLaunchArgument(
+            'use_sim_time', default_value='false',
+            description='Use sim time if true')
+
+    declare_urdf = DeclareLaunchArgument(
+            name='urdf', default_value=urdf_path,
+            description='Path to the robot description file')
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time,'robot_description': Command(['xacro ', urdf])}]
+    )
+
+    return LaunchDescription([declare_urdf,declare_use_sim_time,robot_state_publisher])
